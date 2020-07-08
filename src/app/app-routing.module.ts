1b659bd4a57144cd3b680b09b7ef15e5446251ec
tanayaChavhan <tanayapccoe@gmail.com>
2020-07-08 20:53:39 +0530

    first-angular-bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent1Component } from './test-component1/test-component1.component';
+import { TestComponent2Component } from './test-component2/test-component2.component';
+import { TestComponent3Component } from './test-component3/test-component3.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : " ", pathMatch: "full" ,redirectTo : "/home" },
+  {path : "Home" , component :TestComponent1Component },
+  {path : "Venue" , component : TestComponent2Component},
+  {path : "Locations" , component : TestComponent3Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent1Component } from './test-component1/test-component1.component';
+import { TestComponent2Component } from './test-component2/test-component2.component';
+import { FormsModule } from  '@angular/forms';
+import { TestComponent3Component } from './test-component3/test-component3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent1Component,
+    TestComponent2Component,
+    TestComponent3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
